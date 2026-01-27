63e0779b430954f6e89150e435ce455601b28b31
Alex Campos <alexcampostec@gmail.com>
2026-01-27 19:30:00 -0300

    Enviando ultima parte do estudo bonus SQL

--- Estudo_SQL/Estudo_II/atividade_bonus.sql
+++ Estudo_SQL/Estudo_II/atividade_bonus.sql
@@ -1,14 +1,38 @@
--- Listar os 5 artistas com mais faixas cadastradas.
 
-SELECT
-    tabela_artista.Name AS nome_artista,   -- nome do artista
-    COUNT(tabela_album.AlbumId) AS total_albuns
-FROM artists AS tabela_artista            -- tabela de artistas
-JOIN albums AS tabela_album               -- tabela de álbuns
-    ON tabela_artista.ArtistId = tabela_album.ArtistId
-GROUP BY
-    tabela_artista.ArtistId,
-    tabela_artista.Name
-ORDER BY
-    total_albuns DESC
+SELECT 
+    artists.Name AS Artista, 
+    COUNT(tracks.TrackId) AS Total_Musicas
+FROM artists
+JOIN albums ON artists.ArtistId = albums.ArtistId
+JOIN tracks ON albums.AlbumId = tracks.AlbumId
+GROUP BY artists.Name
+ORDER BY Total_Musicas DESC
 LIMIT 5;
+
+
+-- Listar, para cada país, a quantidade de faturas (Invoice) e o valor médio de Total.
+
+SELECT 
+    BillingCountry, COUNT(InvoiceId) AS Qtde_faturas,
+                    ROUND(AVG(Total), 2) AS media_total
+FROM invoices
+GROUP BY BillingCountry
+ORDER BY Qtde_faturas DESC;
+
+--Descobrir qual país mais gastou em compras.
+
+SELECT BillingCountry, SUM(Total) AS Total_por_pais
+FROM invoices
+GROUP BY BillingCountry
+ORDER BY Total_por_pais DESC
+LIMIT 5;
+
+-- Trazer todas as faixas do gênero “Rock” (veja tabelas Genre e Track,
+-- vai precisar de JOIN se quiser se desafiar).
+
+SELECT 
+    tracks.Name AS Nome_da_Faixa, 
+    genres.Name AS Genero
+FROM tracks
+JOIN genres ON tracks.GenreId = genres.GenreId
+WHERE genres.Name = 'Rock';
